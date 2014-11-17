d66641baa2d53a87bf987a156cd991ece7af0f9b
unscriptable <john@unscriptable.com>
2014-11-16 22:27:12 -0500

    Output some simple log messages.

--- task/createBundle.js
+++ task/createBundle.js
@@ -6,19 +6,31 @@ var context = require('../lib/context');
 var bundle = require('../lib/bundle');
 var loader = require('../lib/loader');
 var when = require('when');
+var build = require('rave/build/build');
 
 module.exports = createBundle;
 
 function createBundle (io, config) {
-	var ctx = context(io)(config);
-	var bndl = ctx.then(bundle(io));
-	var ldr = ctx.then(loader(io));
-	return when.map([ ldr, ctx, bndl ], serializeOne)
+	console.log('Creating complete bundle...');
+	var ctx = context(io)(config)
+		.then(logTap('Created bundled context.'));
+	var bndl = ctx.then(bundle(io))
+		.then(logTap('Created application bundle.'));
+	return when.map([ ctx, bndl ], serializeOne)
+		.then(logTap('Creating complete (rave + context + app) bundle...'))
 		.then(function (serialized) {
-			return serialized.join('\n\n');
+			return build(serialized[0], serialized[1])
+				.then(logTap('Created complete bundle.'));
 		});
 }
 
 function serializeOne (thing) {
 	return thing.serialize();
+}
+
+function logTap (message) {
+	return function (value) {
+		console.log(message);
+		return value;
+	};
 }
